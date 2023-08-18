8b260e80cf8539e7f8360dfdd945aa724a33f79b
Pau <raigroig@hotmail.es>
2023-08-18 13:21:46 +0200

    initial commit

---
+++ content/about.md
@@ -0,0 +1,10 @@
+---
+sitemap:
+  lastmod: 2021-01-01
+  changefreq: monthly
+  priority: 0.8
+---
+
+# About Content v2
+
+[Back home](/)
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <main>
+    <ContentDoc :content="data" />
+
+    <test :data="data" />
+  </main>
+</template>
+
+<script setup>
+/* const { data } = await useAsyncData("home", () =>
+  queryContent("/").only("headline").find()
+) */
+
+const { data } = await useAsyncData("d", () =>
+  queryContent("/").only("headline").find()
+)
+</script>
