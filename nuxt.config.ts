054efc587e43a85f018d10da4c0efa2fbce4b47c
Pau <raigroig@hotmail.es>
2023-08-18 17:43:59 +0200

    chore: add tailwind deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ["@nuxt/content", "nuxt-simple-sitemap"],
+  modules: ["@nuxt/content", "nuxt-simple-sitemap", "@nuxtjs/tailwindcss"],
   content: {
     documentDriven: true,
   },
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,12 @@
 <template>
   <main>
-    <ContentDoc :content="data" />
-
-    <test :data="data" />
+    <ArticlesList :data="data" />
   </main>
 </template>
 
 <script setup>
-/* const { data } = await useAsyncData("home", () =>
-  queryContent("/").only("headline").find()
-) */
 
-const { data } = await useAsyncData("d", () =>
-  queryContent("/").only("headline").find()
+const { data } = await useAsyncData("home", () =>
+  queryContent("/").only(["headline", "_path", "headline", "excerpt"]).find()
 )
 </script>
